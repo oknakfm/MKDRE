652a0e0e1fe127a0c66fa5818e7c419fc0958f77
Akifumi Okuno <77911343+oknakfm@users.noreply.github.com>
2023-11-20 21:55:39 +0900

    Update demonstration.R

--- demonstration.R
+++ demonstration.R
@@ -1,7 +1,5 @@
-require("ks") ## for "kde" function
-require("MASS") ## for "mvrnorm" function
-require("mvtnorm")
-
+require("ks","mvtnorm","rgl") 
+source("functions.R")
 
 set.seed(123)
 
@@ -9,13 +7,10 @@ set.seed(123)
 ## Experimental settings
 par = list(
   nX = 10**4, nY = 10**4,
-  muX = c(0,-0.5), muY = c(0,0.5),
+  muX = c(0,-0.5), muY = c(0,0),
   VX = matrix(c(0.3,0.1,0.1,0.3),2,2),
   VY = matrix(c(0.5,0.1,0.1,0.5),2,2))
 ## =========================================
-
-
-source("functions.R")
 
 n.prec = 15
 
@@ -26,25 +21,20 @@ f = dmvnorm(Z, mean=par$muX, sigma=par$VX)
 g = dmvnorm(Z, mean=par$muY, sigma=par$VY)
 r = f/g
 
-
-
 ## data generation
 X = rmvnorm(n=par$nX, mean=par$muX, sigma=par$VX)
 Y = rmvnorm(n=par$nY, mean=par$muY, sigma=par$VY)
 plot2d(X,Y)
 
-
 ## Indirect KDRE
 I.r = I.KDRE(X=X,Y=Y,Z=Z,h=0.1)
-
 
 ## Direct KDRE
 D.r = D.KDRE(X=X,Y=Y,Z=Z,h=0.1,eps=0.1)
 
 
-
-
-## Plot estimates and true
+## ================================================================
+## Plot estimates and true densities
 pdf(file=paste0(par$nX,"_",par$nY,".pdf"),
     width=4, height=4)
 par(mar = c(4, 4, 2, 1), oma = c(0,0,0,0))
@@ -67,18 +57,28 @@ legend("topleft",legend=c("Indirect","Direct"),
 dev.off()
 
 
-
-
+## ================================================================
+## Contour plot
 pdf(file=paste0("Contour_",par$nX,"_",par$nY,".pdf"),
-    width=12, height=4)
-par(mfrow=c(1,3))
+    width=4, height=4)
 par(mar = c(4, 4, 2, 1), oma = c(0,0,0,0))
 contour(matrix(r, n.prec, n.prec), main="True")
+dev.off()
+
+pdf(file=paste0("Contour_I_",par$nX,"_",par$nY,".pdf"),
+    width=4, height=4)
+par(mar = c(4, 4, 2, 1), oma = c(0,0,0,0))
 contour(matrix(I.r, n.prec, n.prec), main="Indirect")
+dev.off()
+
+pdf(file=paste0("Contour_D_",par$nX,"_",par$nY,".pdf"),
+    width=4, height=4)
+par(mar = c(4, 4, 2, 1), oma = c(0,0,0,0))
 contour(matrix(D.r, n.prec, n.prec), main="Direct")
 dev.off()
 
 
+## ================================================================
 ## 3d plots
 view <- matrix(c(-0.85,0.38,-0.38,0,
                  -0.53,-0.52,0.67,0,
@@ -88,3 +88,4 @@ par3d(userMatrix = view)
 persp3d(x,y,r,col="gray",xlab="z1",ylab="z2",zlab="True")
 persp3d(x,y,I.r,col="gray",xlab="z1",ylab="z2",zlab="Indirect")
 persp3d(x,y,D.r,col="gray",xlab="z1",ylab="z2",zlab="Direct")
+
